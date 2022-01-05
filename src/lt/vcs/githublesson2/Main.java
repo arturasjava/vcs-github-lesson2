bd6585e7b4ca0f60d8cc6257558df83e8b716c23
Arturas <arturas.mikaila@gmail.com>
2022-01-05 18:49:58 +0200

    Initial commit

---
+++ src/lt/vcs/githublesson2/Main.java
@@ -0,0 +1,8 @@
+package lt.vcs.githublesson2;
+
+public class Main {
+  public static void main(String[] args) {
+    System.out.println("Print some text");
+    System.out.println("Some second text");
+  }
+}
