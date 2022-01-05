10f08042ff66b48a6c64d41613ece45e1b2b16b8
Arturas <arturas.mikaila@gmail.com>
2022-01-05 19:45:58 +0200

    Implement new method

--- src/lt/vcs/githublesson2/Main.java
+++ src/lt/vcs/githublesson2/Main.java
@@ -6,5 +6,9 @@ public class Main {
     System.out.println("Some second text");
     System.out.println("Some third text");
     System.out.println("just Print");
+    printSomething();
+  }
+  private static void  printSomething() {
+    System.out.println("Print some Text from method");
   }
 }
