9847d4b0baa2491bbbbebcb746cb43c27b698688
Arturas <arturas.mikaila@gmail.com>
2022-01-05 20:25:15 +0200

    update based on comment

--- src/lt/vcs/githublesson2/UserOne.java
+++ src/lt/vcs/githublesson2/UserOne.java
@@ -3,8 +3,5 @@ package lt.vcs.githublesson2;
 public class UserOne {
   public void printSomeTextUserOne() {
     System.out.println("User one print some text");
-    //Komentaras
-    //Komentaras
-
   }
 }
