46c81c9b396b6ade6532aea88a466ad3af75d47b
Arturas <arturas.mikaila@gmail.com>
2022-01-05 19:11:24 +0200

    Revert "Added just second print text"
    This reverts commit 8172b900

--- src/lt/vcs/githublesson2/Main.java
+++ src/lt/vcs/githublesson2/Main.java
@@ -5,8 +5,6 @@ public class Main {
     System.out.println("Print some text");
     System.out.println("Some second text");
     System.out.println("Some third text");
-    System.out.println("Just Print");
-    System.out.println("Just print 2nd time");
-
+    System.out.println("just Print");
   }
 }
